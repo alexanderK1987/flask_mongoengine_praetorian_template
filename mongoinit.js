ac197fc252d753fe0423a8de4ac3467a3335ab36
alexanderK1987 <tainancgw@gmail.com>
2020-01-13 17:55:41 +0800

    add user deactivation function

--- mongoinit.js
+++ mongoinit.js
@@ -8,3 +8,9 @@ db.createUser({
 
 db.createCollection('users');
 db.createCOllection('revoked_tokens');
+db.createCollection('roles');
+
+db.roles.insert({'role': 'root'});
+db.roles.insert({'role': 'admin'});
+db.roles.insert({'role': 'operator'});
+db.roles.insert({'role': 'guest'});
--- resources/AdminFunction.py
+++ resources/AdminFunction.py
@@ -12,9 +12,9 @@ from models.Users.Revoked_Token_Schema import Revoked_Token_Schema
 @flask_praetorian.auth_required
 @flask_praetorian.roles_accepted('admin')
 def delete_user(user_id):
-    if not User_Model.id_exists(bson.ObjectId(user_id)):
-        return {'msg': 'user does not exists'}, 404
     try:
+        if not User_Model.id_exists(bson.ObjectId(user_id)):
+            return {'msg': 'user does not exists'}, 404
         User_Model.delete_by_id(user_id)
         return '', 204
     except Exception as e:
@@ -36,9 +36,30 @@ def revoke_token():
         jti = mainframe.guard.extract_jwt_token(token)['jti']
         if Revoked_Token_Model.is_jti_revoked(jti):
             return {'msg': 'token already revoked (%s)' % (token)}, 400
-
         new_revocation = Revoked_Token_Schema().load({'jti': jti}).data
         new_revocation.save()
         return {'msg': 'revocation successful (%s)' % (token)}
     except Exception as e:
-        return {'msg': 'Something went wrong', 'detail': str(e)}, 500 
+        return {'msg': 'Something went wrong', 'detail': str(e)}, 500
+
+@mainframe.app.route('/deactivate_user/<user_id>')
+@flask_praetorian.auth_required
+@flask_praetorian.roles_accepted('admin')
+def deactivate_user(user_id):
+    try:
+        if not User_Model.id_exists(bson.ObjectId(user_id)):
+            return {'msg': 'user does not exists'}, 404
+        user = User_Model.get_by_id(user_id)
+        # forbid to deactive root user
+        if "root" in user.roles:
+            return {'msg': 'deactivation of root user %s is forbidden' % (user.email)}, 403
+        
+        # user already deactivated or never activated
+        if not user.active:
+            return {'msg': 'user %s is already deactivated' % (user.email)}, 403
+
+        user.active = False
+        user.save()
+        return {'msg': 'user %s deactivated' % (user.email)}, 200
+    except Exception as e:
+        return {'msg': 'Something went wrong', 'detail': str(e)}, 500
