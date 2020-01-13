98ebb685c7cca233f2bff0f0f0445b9c24331218
alexanderK1987 <tainancgw@gmail.com>
2020-01-13 10:59:37 +0800

    move super_user profile to app.config

--- init_finalize.py
+++ init_finalize.py
@@ -3,14 +3,9 @@ import mainframe
 from models.Users.User_Model import  User_Model
 from models.Users.User_Schema import User_Schema
 
-super_user_profile = {'email': 'admin@memphis.gov',
-                      'password': mainframe.guard.hash_password('memphis'),
-                      'roles': ['admin'],
-                      'active': True,
-                      'last_login': str(datetime.datetime.now()),
-                      'origin': str(datetime.datetime.now())}
-
+super_user_profile = mainframe.app.config['SUPER_USER_PROFILE']
 super_user = User_Schema().load(super_user_profile).data
+super_user.password = mainframe.guard.hash_password(super_user.password)
 
 if not User_Model.email_exists(super_user.email):
     print ('super user does not exist, inserting ...')
