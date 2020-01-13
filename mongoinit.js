984e0ddefea927454db76d84e80111cc8ea2b8d8
alexanderK1987 <tainancgw@gmail.com>
2020-01-13 11:15:58 +0800

    regularise power user

--- mongoinit.js
+++ mongoinit.js
@@ -1,8 +1,6 @@
 db=db.getSiblingDB('admin')
 db=db.getSiblingDB('memphis')
-db.createUser({ user: "memphis_admin",pwd: "memphis",roles: [ { role: "readWrite", db: "memphis" } ]});
+db.createUser({ user: "memphis_admin",pwd: "memphis",roles: [{role: "readWrite", db: "memphis" }]});
 
 db.createCollection('users');
-db.createCollection('power_users');
-db.power_users.insert({'email': 'k_101@live.com', 'password': 'k_101@live.com'});
 
